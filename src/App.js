0e2e8dd3e6ae3c7f260d477b83e88fd990ecef29
Christian Franco <chris94franco06@gmail.com>
2026-06-06 18:29:03 -0500

    clase #4

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
 import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  {text: 'Cortar cebolla', completed: true},
+  {text: 'Tomar curso de React.js', completed: false},
+  {text: 'Llorar con la Llorona', completed: false},
+  {text: 'LALALALALALA', completed: false},
+];
 
 function App() {
   return (
-    <div className="App">
-     
-     <TodoCounter />
-     <TodoSearch />
+   <React.Fragment>  
+        <TodoCounter completed={16} total={25} />
+        <TodoSearch />
 
-     <TodoList />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-    <TodoList />
-
-     
-    </div>
+        <TodoList />
+           {defaultTodos.map(todo => (
+            <TodoItem 
+              key={todo.text} 
+              text={todo.text} 
+              completed={todo.completed}
+            />
+           ))}
+        <TodoList />
+        
+      <CreateTodoButton />  
+   </React.Fragment> 
   );
 }
 
-function TodoItem() {
-  return(
-    <li>
-      <span>V</span>
-      <p>Llorar con la Llorona</p>
-      <span>X</span>
-    </li>
-  );
-}
+
 
 
 export default App;

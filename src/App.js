f0e64d91077aac0472dca5605b448c5a4592d058
Chris06ran <chris06franco94@gmail.com>
2026-06-10 17:26:48 -0500

    clase 5 - 16:06

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
-import './App.css';
 import React from 'react';
 
 const defaultTodos = [
@@ -15,7 +14,7 @@ const defaultTodos = [
 
 function App() {
   return (
-   <React.Fragment>  
+   <>  {/*FORMA DE ABREVIAR LOS FRAGMENTS */}
         <TodoCounter completed={16} total={25} />
         <TodoSearch />
 
@@ -29,8 +28,7 @@ function App() {
            ))}
         <TodoList />
         
-      <CreateTodoButton />  
-   </React.Fragment> 
+      </>  
   );
 }
 

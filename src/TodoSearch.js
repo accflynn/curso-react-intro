26cb143e2a4781d322b9fde1c16b560ce0e83812
Chris06ran <chris06franco94@gmail.com>
2026-06-17 13:15:20 -0500

    clase 6

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -4,6 +4,12 @@ function TodoSearch() {
   return(
     <input placeholder="Cortar cebolla"
      className="TodoSearch"
+      onChange={(event) => {
+        console.log('Escribiste en el TodoSearch');
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
     />
   );
 }

526a15501e2185d52c0a36a9fba11aa400ce54fe
Chris06ran <chris06franco94@gmail.com>
2026-07-22 13:35:28 -0500

    clase 13 y 12

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,24 +5,20 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <CompleteIcon />
-      {/* <span className={`Icon Icon-check ${props.
-      completed && "Icon-check--active"}`}
-      onClick={props.onComplete}
-      >
-        V
-      </span> */}
+      <CompleteIcon
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
+        
+  
       <p className={`TodoItem-p ${props.
         completed && "TodoItem-p--completed"}`}>
         {props.text}
       </p>
-      <DeleteIcon />
-      {/* <span 
-      className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >
-        X 
-      </span> */}
+      <DeleteIcon
+        onDelete={props.onDelete}
+      />
+      
     </li>
   );
 }
